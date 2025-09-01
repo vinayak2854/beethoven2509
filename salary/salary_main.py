774c45c6e1f42c0b27bdb8dc6400858476fd2990
vinayak2854 <vinayak.vallakati@gmail.com>
2025-09-01 16:41:16 +0530

    manager menu

--- salary/salary_main.py
+++ salary/salary_main.py
@@ -2,22 +2,46 @@ from salary_manager import create_salary, read_all, read_by_salary
 from salary_manager import salaries, update, delete_by_salary
 
 
-create_salary(10000)
-create_salary(20000)     
-create_salary(30000) 
-create_salary(40000) 
+def menu():
+    message = '''
+1 - create salary
+2 - read all salaries
+3 - read by salary
+4 - update
+5 - delete
+6- exit / logout
+'''
+    choice = int(input(message))
+    if choice == 1:
+        salary = int (input('salary:'))
+        create_salary(salary)
+    elif choice == 2:
+        result_salaries = read_all()
+        print('salaries:')
+        for salary in result_salaries:
+            print(salary)
+    elif choice == 3:
+        salary = int(input('search salary:'))
+        index + read_by_salary(salary)
+        if salary == -1:
+            print('salary not found')
+        else:
+            print(f'salary is at index {index}')
+    elif choice == 4:
+        old_salary = int(input('salary to update:'))
+        new_salary = int(input('new salary:'))
+        update(salary, new_salary)
+    elif choice == 5:
+        salary = int(input('salary to delete:'))
+        delete_by_salary(salary)
+    return choice  
 
-result_salaries = read_all()
-for salary in result_salaries:
-    print(salary)
 
-print(read_by_salary(20000))
-print(read_by_salary(50000))
-print(salaries[read_by_salary(40000)])
-
-
-update(30000, 300)
-print(read_all())
-
-delete_by_salary(10000)
-print(read_all())
+def menus():
+    print('Salary Management App') 
+    choice = menu()
+    while choice != 6:
+        choice = menu()
+    print('Thank you for using app')    
+    
+menus()
